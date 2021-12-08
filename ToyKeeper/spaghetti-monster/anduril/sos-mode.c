f88214ed3fc16ca8f7fd53583529627aad8712a8
Bi Cheng Wu <bwu62@wisc.edu>
2021-12-08 03:53:20 -0600

    tweak SOS mode timings

--- ToyKeeper/spaghetti-monster/anduril/sos-mode.c
+++ ToyKeeper/spaghetti-monster/anduril/sos-mode.c
@@ -45,7 +45,7 @@ uint8_t sos_state(Event event, uint16_t arg) {
 #endif
 
 void sos_blink(uint8_t num, uint8_t dah) {
-    #define DIT_LENGTH 200
+    #define DIT_LENGTH 125
     for (; num > 0; num--) {
         set_level(memorized_level);
         nice_delay_ms(DIT_LENGTH);
@@ -67,7 +67,7 @@ inline void sos_mode_iter() {
     sos_blink(3, 0);  // S
     sos_blink(3, 1);  // O
     sos_blink(3, 0);  // S
-    nice_delay_ms(2000);
+    nice_delay_ms(DIT_LENGTH*5);
 }
 
 
